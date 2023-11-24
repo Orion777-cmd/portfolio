83429565a1227cbb42e5057beb28d8627f06094b
Orion777-cmd <abiy.biru78@gmail.com>
2023-11-24 21:09:14 +0300

    added the skeleton of the code

---
+++ Portfolio-website/src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+      <div className="container ">
+        <div className="profile-container">
+          <div className="profile">
+            <img src="https://avatars.githubusercontent.com/u/61617706?v=4" alt="profile" />
+          </div>
+          <div className="profile-name">
+            <h2>Shubham Singh</h2>
+          </div>
+          <div className="profile-description">
+            <p>Hey, I'm Shubham. I'm a web developer. I'm currently working on React and Typescript. I'm also learning Rust and Web Assembly.</p>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default App
