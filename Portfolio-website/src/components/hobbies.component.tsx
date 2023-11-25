5270af7d011204c8f92429d2d7aff1d1f622ea53
Orion777-cmd <abiy.biru78@gmail.com>
2023-11-25 22:28:40 +0300

    added a hobbies component

---
+++ Portfolio-website/src/components/hobbies.component.tsx
@@ -0,0 +1,14 @@
+
+const Hobbies = () => {
+    return(
+        <div className="flex flex-wrap justify-center items-center m-2 p-2 h-30 border-t-2 border-black">
+            <p>
+                Exploring the harmonies of music, the cinematic world, and the written narratives in novels enriches my leisure. Meanwhile,
+                having fun with sketching and diving into the lively play of table tennis adds an extra layer of enjoyment to my daily life.
+            </p>
+
+        </div>
+    )
+};
+
+export default Hobbies;
--- Portfolio-website/src/components/profile.component.tsx
+++ Portfolio-website/src/components/profile.component.tsx
@@ -1,5 +1,6 @@
 
 import Social from "./socials.component";
+import Hobbies from "./hobbies.component";
 import profile from "../assets/the_guitar.jpg";
 
 const Profile = () => {
@@ -8,13 +9,14 @@ const Profile = () => {
         <div className="flex xl:flex-row flex-col gap-2 justify-around items-center">
           <img src={profile} alt="" className="w-[200px] h-[200px] rounded-full object-cover object-center" />
           <div className="details flex flex-col items-center justify-center ">
-            <h1 className="text-2xl text-left">Hi, I am Abiy</h1>
+            <h1 className="text-2xl text-left">Hi &#128075;, I am Abiy</h1>
             
             <h2 className="text-base text-center">A Full-stack and Mobile app developer.</h2>
           </div>
         </div>
+        <Hobbies />
         <Social />
-      
+
       </div>
     )
 }
