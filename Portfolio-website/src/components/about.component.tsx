7e91bbacf2ba284775e8e09f239807aa9d5f2e05
Orion777-cmd <abiy.biru78@gmail.com>
2023-11-25 14:50:19 +0300

    changed some style

--- Portfolio-website/src/components/about.component.tsx
+++ Portfolio-website/src/components/about.component.tsx
@@ -2,9 +2,9 @@
 
 const About = () => {
     return (
-        <div className="bg-orange-100 w-1/2 m-5 p-4 border-black shadow-2xl flex flex-col gap-4 justify-center items-center">
-            <h1 className="text-2xl font-bold text-center">About me</h1>
-            <p className="text-left mx-auto">Fueled by an unyielding curiosity about how technology shapes our lives, my journey naturally evolved into the vibrant world of software development. Here, I thrive as a full-stack and mobile app developer, driven by a genuine passion for continual learning. I genuinely enjoy seizing every opportunity to openly share insights. If something I create resonates with you, let's connect and chat about it on my social platforms. Your thoughts and feedback are always welcomed and valued.</p>
+        <div className="bg-black w-1/2 m-5 p-4 border-black shadow-2xl shadow-black flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-2xl text-white font-bold text-center">About me</h1>
+            <p className="text-left text-white mx-auto">Fueled by an unyielding curiosity about how technology shapes our lives, my journey naturally evolved into the vibrant world of software development. Here, I thrive as a full-stack and mobile app developer, driven by a genuine passion for continual learning. I genuinely enjoy seizing every opportunity to openly share insights. If something I create resonates with you, let's connect and chat about it on my social platforms. Your thoughts and feedback are always welcomed and valued.</p>
         </div>
     )
 }
--- Portfolio-website/src/components/profile.component.tsx
+++ Portfolio-website/src/components/profile.component.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return(
         <div className="container w-1/2 m-5 p-2 border-black shadow-lg flex flex-col ">
         <div className="flex justify-around items-center">
-          <img src={profile} alt="" className="w-40 rounded-full" />
+          <img src={profile} alt="" className="w-[200px] h-[200px] rounded-full object-cover object-center" />
           <div className="details flex flex-col items-center justify-center ">
             <h1 className="text-2xl text-left">Hi, I am Abiy</h1>
             
