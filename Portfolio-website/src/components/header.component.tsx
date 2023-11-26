eca3b312a5ad8dedeb5f488f7bd4c2d0b0ae0072
Orion777-cmd <abiy.biru78@gmail.com>
2023-11-26 17:36:41 +0300

    added some sytyling changes

--- Portfolio-website/src/components/header.component.tsx
+++ Portfolio-website/src/components/header.component.tsx
@@ -4,10 +4,10 @@ import { useDarkMode} from "../context/darkmode.context";
 const Header = () => {
     const {darkMode, setDarkMode} = useDarkMode();
     return (
-        <div className="mx-auto pt-10 w-4/5 header flex flex-col lg:flex-row justify-between items-center ">
+        <div className="mx-auto mb-7 pt-10 w-4/5 header flex flex-col lg:flex-row justify-between items-center ">
           <h1 className='text-4xl bold'>Abiy B. Bihonegn</h1>
           <div className=' w-2/5  flex items-center justify-around'>
-            <a href="" className='text-bold text-4xl '>Resume</a>
+            <a href="" className={`text-bold text-2xl border-2 px-2 rounded-full ${darkMode? 'border-white': 'border-black'} shadow-md ${darkMode ? 'hover:shadow-white' : 'hover:shadow-black'}`}>Resume</a>
             {darkMode ?  <CiLight className="text-2xl" onClick={() => setDarkMode(!darkMode)} /> : <MdDarkMode className="text-2xl" onClick={() => setDarkMode(!darkMode)} />}
           </div>
         </div>
