455865db32fc0ca4b9989dfb4cbb014ce1814aad
Orion777-cmd <abiy.biru78@gmail.com>
2023-11-26 17:25:17 +0300

    fixed responsiveness issues

--- Portfolio-website/src/components/header.component.tsx
+++ Portfolio-website/src/components/header.component.tsx
@@ -4,9 +4,9 @@ import { useDarkMode} from "../context/darkmode.context";
 const Header = () => {
     const {darkMode, setDarkMode} = useDarkMode();
     return (
-        <div className="mx-auto pt-10 w-4/5 header flex justify-between items-center ">
-          <h1 className='text-4xl bold'>Abiy Biru</h1>
-          <div className=' w-1/4  flex items-center justify-between'>
+        <div className="mx-auto pt-10 w-4/5 header flex flex-col lg:flex-row justify-between items-center ">
+          <h1 className='text-4xl bold'>Abiy B. Bihonegn</h1>
+          <div className=' w-2/5  flex items-center justify-around'>
             <a href="" className='text-bold text-4xl '>Resume</a>
             {darkMode ?  <CiLight className="text-2xl" onClick={() => setDarkMode(!darkMode)} /> : <MdDarkMode className="text-2xl" onClick={() => setDarkMode(!darkMode)} />}
           </div>
